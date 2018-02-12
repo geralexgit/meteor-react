159875a0e3b5e3e0734bfabaefea494f16e65eaa
User <geralexndr@gmail.com>
2018-02-12 15:10:22 +0300

    chips added to card

--- impors/ui/Player.jsx
+++ impors/ui/Player.jsx
@@ -1,24 +1,75 @@
 import React from 'react';
-import { Card, CardActions, CardMedia, CardTitle, CardText } from 'material-ui/Card';
-import FlatButton from 'material-ui/FlatButton';
+import {Card, CardActions, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
+import Avatar from 'material-ui/Avatar';
+import Chip from 'material-ui/Chip';
+import {blue200, blue900} from 'material-ui/styles/colors';
+
+const styles = {
+  button: {
+    margin: 12
+  },
+  chip: {
+    margin: 4
+  },
+  wrapper: {
+    display: "flex",
+    flexWrap: "wrap"
+  }
+};
 
 const Player = () => (
-    <Card>
-        <CardMedia
-            overlay={<CardTitle title="Some Name" subtitle="text text text" />}
-        >
-            <img src="http://www.fillmurray.com/g/300/300" alt="" />
-        </CardMedia>
-        <CardTitle title="Card title" subtitle="Card subtitle" />
-        <CardText>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+  <Card>
+    <CardMedia overlay={< CardTitle title = "Some Name" subtitle = "text text text" />}>
+      <img src="http://www.fillmurray.com/g/300/300" alt="" />
+    </CardMedia>
+    <CardTitle title="Card title" subtitle="Card subtitle" />
+    <CardText>
+      <Chip backgroundColor={blue200} style={styles.chip}>
+        <Avatar size={32} color={blue200} backgroundColor={blue900}>
+          3
+        </Avatar>
+        Ball manipulation
+      </Chip>
+      <Chip backgroundColor={blue200} style={styles.chip}>
+        <Avatar size={32} color={blue200} backgroundColor={blue900}>
+          3
+        </Avatar>
+        Kicking Abilities
+      </Chip>
+      <Chip backgroundColor={blue200} style={styles.chip}>
+        <Avatar size={32} color={blue200} backgroundColor={blue900}>
+          3
+        </Avatar>
+        Passing Abilities
+      </Chip>
+      <Chip backgroundColor={blue200} style={styles.chip}>
+        <Avatar size={32} color={blue200} backgroundColor={blue900}>
+          3
+        </Avatar>
+        Ball manipulation
+      </Chip>
+      <Chip backgroundColor={blue200} style={styles.chip}>
+        <Avatar size={32} color={blue200} backgroundColor={blue900}>
+          3
+        </Avatar>
+        Ball manipulation
+      </Chip>
+      <Chip backgroundColor={blue200} style={styles.chip}>
+        <Avatar size={32} color={blue200} backgroundColor={blue900}>
+          3
+        </Avatar>
+        Game startegy
+      </Chip>
+      <Chip backgroundColor={blue200} style={styles.chip}>
+        <Avatar size={32} color={blue200} backgroundColor={blue900}>
+          3
+        </Avatar>
+        Risk
+      </Chip>
     </CardText>
-        <CardActions>
-        </CardActions>
-    </Card>
+    <CardActions></CardActions>
+  </Card>
 )
 
 export default Player;
