79a24c1ffcef96deb1f5aba2eeeb04666a26040a
User <geralexndr@gmail.com>
2018-02-12 14:48:57 +0300

    card component added

--- impors/ui/Player.jsx
+++ impors/ui/Player.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Card, CardActions, CardMedia, CardTitle, CardText } from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 
 const Player = () => (
-    <h1>Player</h1>
+    <Card>
+        <CardMedia
+            overlay={<CardTitle title="Some Name" subtitle="text text text" />}
+        >
+            <img src="http://www.fillmurray.com/g/300/300" alt="" />
+        </CardMedia>
+        <CardTitle title="Card title" subtitle="Card subtitle" />
+        <CardText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+    </CardText>
+        <CardActions>
+        </CardActions>
+    </Card>
 )
 
 export default Player;
